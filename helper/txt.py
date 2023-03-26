744826c89ca12a3faaf17499478b1ced06c78997
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-03-26 16:55:34 +0530

    CC Added

--- helper/txt.py
+++ helper/txt.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2023 DX_MODS
+#Licensed under the  AGPL-3.0 License;
+#you may not use this file except in compliance with the License.
+#Author ZIYAN"
+
 class mr(object):
     PROGRESS_BAR = """\n
 ╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
