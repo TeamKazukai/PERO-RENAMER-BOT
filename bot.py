901d125b98f51662bc74c2afe77e3d3e54330958
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-09 16:43:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,9 +46,5 @@ class Bot(Client):
             except:
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
 
-    async def stop(self, *args):
-        await super().stop()      
-        print("Bᴏᴛ Sᴛᴏᴩᴩᴇᴅ.. Byᴇ")
-       
-
 Bot().run()
+       
