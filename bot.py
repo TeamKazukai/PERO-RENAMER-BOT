af93c6e597ff60df6e5e467203c0f481cf5b67ac
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-07-09 19:39:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,10 @@ from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
 from route import web_server
+import asyncio
 from helper.ping_server import ping_server
+
+
 class Bot(Client):
 
     def __init__(self):
