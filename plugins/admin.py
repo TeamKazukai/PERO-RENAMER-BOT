4fd9e87993c0a62c9e2fc667cfaf29461d4938c6
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-09-08 21:10:52 +0530

    Fix✅

--- plugins/admin.py
+++ plugins/admin.py
@@ -6,11 +6,11 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+from config import Config
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
+@Client.on_message(filters.private & filters.user(Config.ADMIN) & filters.command(["warn"]))
 async def warn(c, m):
         if len(m.command) >= 3:
             try:
@@ -22,7 +22,7 @@ async def warn(c, m):
                  await m.reply_text("User Not Notfied Sucessfully 😔") 
 
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
+@Client.on_message(filters.private & filters.user(Config.ADMIN) & filters.command(["addpremium"]))
 async def buypremium(bot, message):
 	await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("VIP 1",callback_data = "vip1"), 
