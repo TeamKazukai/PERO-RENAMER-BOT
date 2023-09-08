93db32110bbbe5f14eb89de0f401c14e24b0d29c
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-09-08 18:52:56 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,99 +3,179 @@
 #you may not use this file except in compliance with the License.
 #Author ZIYAN
 
-import random
+import os
+import pymongo
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+import time
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
-from helper.database import db
-from config import Config, Txt  
-  
+from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
+import humanize
+from helper.progress import humanbytes
 
-@Client.on_message(filters.private & filters.command("start"))
-async def start(client, message):
-    user = message.from_user
-    await db.add_user(client, message)                
-    button = InlineKeyboardMarkup([[
-        InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
-        ],[
-        InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/dxmodsupdates'),
-        InlineKeyboardButton('💁‍♂️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/DXMODS_Support')
-        ],[
-        InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
-        InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
-    ]])
-    if Config.START_PIC:
-        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
-    else:
-        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
-   
+from helper.database import  insert ,find_one,used_limit,usertype,uploadlimit,addpredata,total_rename,total_size,usertype,backpre
+from pyrogram.file_id import FileId
+from helper.database import daily as daily_
+from helper.date import add_date ,check_expi
+CHANNEL = os.environ.get('CHANNEL',"")
+import datetime
+from datetime import date as date_
+STRING = os.environ.get("STRING","")
+log_channel = int(os.environ.get("LOG_CHANNEL",""))
+token = os.environ.get('TOKEN','')
+botid = token.split(':')[0]
 
-@Client.on_callback_query()
-async def cb_handler(client, query: CallbackQuery):
-    data = query.data 
-    if data == "start":
-        await query.message.edit_text(
-            text=Txt.START_TXT.format(query.from_user.mention),
-            disable_web_page_preview=True,
-            reply_markup = InlineKeyboardMarkup([[
-                InlineKeyboardButton("👨‍💻 Dᴇᴠꜱ 👨‍💻", callback_data='dev')
-                ],[
-                InlineKeyboardButton('📯 Uᴩᴅᴀᴛᴇꜱ', url='https://t.me/dxmodsupdates'),
-                InlineKeyboardButton('💁‍♂️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/DXMODS_Support')
-                ],[
-                InlineKeyboardButton('🎛️ Aʙᴏᴜᴛ', callback_data='about'),
-                InlineKeyboardButton('🛠️ Hᴇʟᴩ', callback_data='help')
-            ]])
-        )
-    elif data == "help":
-        await query.message.edit_text(
-            text=Txt.HELP_TXT,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[
-                #⚠️ don't change source code & source link ⚠️ #
-                InlineKeyboardButton("❣️ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/DX-MODS/BIXBY-RENAME-BOT")
-                ],[
-                InlineKeyboardButton("❤️‍🔥 Hᴏᴡ Tᴏ Uꜱᴇ❤️‍🔥", url='https://github.com/DX-MODS/BIXBY-RENAME-BOT')
-                ],[
-                InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close"),
-                InlineKeyboardButton("◀️ Bᴀᴄᴋ", callback_data = "start")
-            ]])            
-        )
-    elif data == "about":
-        await query.message.edit_text(
-            text=Txt.ABOUT_TXT.format(client.mention),
-            disable_web_page_preview = True,
-            reply_markup=InlineKeyboardMarkup([[
-                #⚠️ don't change source code & source link ⚠️ #
-                InlineKeyboardButton("❣️ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/DX-MODS/BIXBY-RENAME-BOT")
-                ],[
-                InlineKeyboardButton("🖥️ Hᴏᴡ Tᴏ Mᴀᴋᴇ", url="https://github.com/DX-MODS/BIXBY-RENAME-BOT")
-                ],[
-                InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close"),
-                InlineKeyboardButton("◀️ Bᴀᴄᴋ", callback_data = "start")
-            ]])            
-        )
-    elif data == "dev":
-        await query.message.edit_text(
-            text=Txt.DEV_TXT,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[
-                #⚠️ don't change source code & source link ⚠️ #
-                InlineKeyboardButton("❣️ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ", url="https://github.com/DX-MODS/BIXBY-RENAME-BOT")
-                ],[
-                InlineKeyboardButton("🖥️ Hᴏᴡ Tᴏ Mᴀᴋᴇ", url="https://github.com/DX-MODS/BIXBY-RENAME-BOT")
-                ],[
-                InlineKeyboardButton("🔒 Cʟᴏꜱᴇ", callback_data = "close"),
-                InlineKeyboardButton("◀️ Bᴀᴄᴋ", callback_data = "start")
-            ]])          
-        )
-    elif data == "close":
-        try:
-            await query.message.delete()
-            await query.message.reply_to_message.delete()
-            await query.message.continue_propagation()
-        except:
-            await query.message.delete()
-            await query.message.continue_propagation()
+DB_NAME = os.environ.get("DB_NAME","")
+DB_URL = os.environ.get("DB_URL","")
+mongo = pymongo.MongoClient(DB_URL)
+db = mongo[DB_NAME]
+dbcol = db["promo"]
+
+def profind(id):
+	return dbcol.find_one({"_id":id})
+
+
+#Part of Day --------------------
+currentTime = datetime.datetime.now()
+
+if currentTime.hour < 12:
+	wish = "Good morning."
+elif 12 <= currentTime.hour < 12:
+	wish = 'Good afternoon.'
+else:
+	wish = 'Good evening.'
+
+#-------------------------------
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+	old = insert(int(message.chat.id))
+	user_id = message.from_user.id
+	letdata = profind(int(user_id))
+	try:
+	    procode = letdata["promo"]
+	except:
+	    pass	
+	try:
+	    id = message.text.split(' ')[1]
+	except:
+	    await message.reply_text(text =f"""
+	Hello {wish} {message.from_user.first_name }
+	__I am file renamer bot, Please sent any telegram 
+	**Document Or Video** and enter new filename to rename it__
+	""",reply_to_message_id = message.id ,  
+	reply_markup=InlineKeyboardMarkup(
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
+	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	    return
+	if id:
+	        if id == procode:
+	            await message.reply_text("You Can Use Now ")
+	            uploadlimit(int(user_id),10737418240)
+	            usertype(int(user_id),"NORMAL")
+@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
+async def send_doc(client,message):
+       update_channel = CHANNEL
+       user_id = message.from_user.id
+       if update_channel :
+       	try:
+       		await client.get_chat_member(update_channel, user_id)
+       	except UserNotParticipant:
+       		await message.reply_text("**__You are not subscribed my channel__** ",
+       		reply_to_message_id = message.id,
+       		reply_markup = InlineKeyboardMarkup(
+       		[ [ InlineKeyboardButton("Support 🇮🇳" ,url=f"https://t.me/{update_channel}") ]   ]))
+       		return
+       try:
+           bot_data = find_one(int(botid))
+           prrename = bot_data['total_rename']
+           prsize = bot_data['total_size']
+           user_deta = find_one(user_id)
+       except:
+           await message.reply_text("Use About cmd first /about")
+       try:
+       	used_date = user_deta["date"]
+       	buy_date= user_deta["prexdate"]
+       	daily = user_deta["daily"]
+       	user_type = user_deta["usertype"]
+       except:
+           await message.reply_text("database has been Cleared click on /start")
+           return
+           
+           
+       c_time = time.time()
+       
+       if user_type=="Free":
+           LIMIT = 600
+           await message.reply_text(f"Complete The Task and get Free subscription per day . Watch full video https://lntechnical.works/{message.from_user.id} ")
+           return
+       elif user_type=="NORMAL":
+           LIMIT = 250
+       else:
+           LIMIT = 30
+       then = used_date+ LIMIT
+       left = round(then - c_time)
+       conversion = datetime.timedelta(seconds=left)
+       ltime = str(conversion)
+       if left > 0:       	    
+       	await message.reply_text(f"```Sorry Dude I am not only for YOU \n Flood control is active so please wait for {ltime}```",reply_to_message_id = message.id)
+       else:
+       		await client.forward_messages(log_channel, message.from_user.id, message.id)
+       		await client.send_message(log_channel,f"User Id :- {user_id}")       		
+           		
+       		media = await client.get_messages(message.chat.id,message.id)
+       		file = media.document or media.video or media.audio 
+       		dcid = FileId.decode(file.file_id).dc_id
+       		filename = file.file_name
+       		value = 2147483648
+       		used_ = find_one(message.from_user.id)
+       		used = used_["used_limit"]
+       		limit = used_["uploadlimit"]
+       		expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
+       		if expi != 0:
+       			today = date_.today()
+       			pattern = '%Y-%m-%d'
+       			epcho = int(time.mktime(time.strptime(str(today), pattern)))
+       			daily_(message.from_user.id,epcho)
+       			used_limit(message.from_user.id,0)
+       			if user_type == "NORMAL":
+       				usertype(message.from_user.id,"Free")
+			     		
+       		remain = limit- used
+       		if remain < int(file.file_size):
+       		    await message.reply_text(f"Sorry! I can't upload files that are larger than {humanbytes(limit)}. File size detected {humanbytes(file.file_size)}\nUsed Daly Limit {humanbytes(used)} If U Want to Rename Large File Upgrade Your Plan ",reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade") ]]))
+       		    return
+       		if value < file.file_size:
+       		    if STRING:
+       		        if buy_date==None:
+       		            await message.reply_text(f" You Can't Upload More Then {humanbytes(limit)} Used Daly Limit {humanbytes(used)} ",reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upgrade 💰💳",callback_data = "upgrade") ]]))
+       		            return
+       		        pre_check = check_expi(buy_date)
+       		        if pre_check == True:
+       		            await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {humanize.naturalsize(file.file_size)}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
+       		            total_rename(int(botid),prrename)
+       		            total_size(int(botid),prsize,file.file_size)
+       		        else:
+       		            backpre(message.from_user.id)
+	
+       		            await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
+       		            return
+       		    else:
+       		          	await message.reply_text("Can't upload files bigger than 2GB ")
+       		          	return
+       		else:
+       		    if buy_date:
+       		        pre_check = check_expi(buy_date)
+       		        if pre_check == False:
+       		            backpre(message.from_user.id)       		            
+       		        
+       		    filesize = humanize.naturalsize(file.file_size)
+       		    fileid = file.file_id
+       		    total_rename(int(botid),prrename)
+       		    total_size(int(botid),prsize,file.file_size)
+       		    await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup(
+       		[[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),
+       		InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
+       		
 
 
 
