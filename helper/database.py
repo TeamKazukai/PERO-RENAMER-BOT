ffd4ae2aa02fe700dccb042578fc1296d38c5af4
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-09-08 20:25:58 +0530

    Fix

--- helper/database.py
+++ helper/database.py
@@ -21,42 +21,103 @@ class Database:
             caption=None
         )
 
-    async def add_user(self, b, m):
-        u = m.from_user
-        if not await self.is_user_exist(u.id):
-            user = self.new_user(u.id)
-            await self.col.insert_one(user)            
-            await send_log(b, u)
+    #Total User 
 
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return bool(user)
+def total_user():
+      user = self.col.count_documents({})
+      return user
+      
+#insert bot Data 
+def botdata(chat_id):
+	bot_id = int(chat_id)
+	try:
+		bot_data = {"_id":bot_id,"total_rename":0,"total_size":0}
+		dbcol.insert_one(bot_data)
+	except:
+		pass
 
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
 
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
+def total_rename(chat_id,renamed_file):
+	now = int(renamed_file) + 1
+	self.col.update_one({"_id":chat_id},{"$set":{"total_rename":str(now)}})
+	
+def total_size(chat_id,total_size,now_file_size):
+	now = int(total_size) + now_file_size
+	self.col.update_one({"_id":chat_id},{"$set":{"total_size":str(now)}})
 
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'_id': int(user_id)})
+	
+#insert user data 
+def insert(chat_id):
+            user_id = int(chat_id)
+            user_det = {"_id":user_id,"file_id":None ,"caption":None ,"daily":0 ,"date":0 , "uploadlimit" :2147483648,"used_limit":0,"usertype":"Free","prexdate" : None}
+            try:
+            	self.col.insert_one(user_det)
+            except:
+            	return True
+            	pass
+
+def addthumb(chat_id, file_id):
+	self.col.update_one({"_id":chat_id},{"$set":{"file_id":file_id}})
+	
+def delthumb(chat_id):
+	self.col.update_one({"_id":chat_id},{"$set":{"file_id":None}})
+
+def addcaption(chat_id, caption):
+       self.col.update_one({"_id": chat_id},{"$set":{"caption": caption}})
+	
+def delcaption(chat_id): 
+        self.col.update_one({"_id": chat_id},{"$set":{"caption":None}})
+	
+def dateupdate(chat_id,date):
+	self.col.update_one({"_id":chat_id},{"$set":{"date":date}})
+
+def used_limit(chat_id,used):
+	self.col.update_one({"_id":chat_id},{"$set":{"used_limit":used}})
+	
+def usertype(chat_id,type):
+	self.col.update_one({"_id":chat_id},{"$set":{"usertype":type}})
+	
+def uploadlimit(chat_id,limit):
+	self.col.update_one({"_id":chat_id},{"$set":{"uploadlimit":limit}})
+
+def backpre(chat_id):
+    self.col.update_one({"_id":chat_id},{"$set":{"prexdate":None}})
     
-    async def set_thumbnail(self, id, file_id):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'file_id': file_id}})
+def addpre(chat_id):
+    date = add_date()
+    self.col.update_one({"_id":chat_id},{"$set":{"prexdate":date[0]}})
 
-    async def get_thumbnail(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('file_id', None)
+def addpredata(chat_id):
+    self.col.update_one({"_id":chat_id},{"$set":{"prexdate":None}})
+    
+  
+def daily(chat_id,date):
+	  self.col.update_one({"_id":chat_id},{"$set":{"daily":date}})
+	  
+def find(chat_id):
+	id =  {"_id":chat_id}
+	x = self.col.find(id)
+	for i in x:
+             file = i["file_id"]
+             try:
+                 caption = i["caption"]
+             except:
+                 caption = None
+                 
+             return [file, caption]
 
-    async def set_caption(self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+def getid():
+    values = []
+    for key  in self.col.find():
+         id = key["_id"]
+         values.append((id)) 
+    return values
 
-    async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
-
+def delete(id):
+	self.col.delete_one(id)
+	
+def find_one(id):
+	return self.col.find_one({"_id":id})
 
 db = Database(Config.DB_URL, Config.DB_NAME)
 
