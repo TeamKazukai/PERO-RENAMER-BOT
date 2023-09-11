5b9772f59ef28987ad576ac4521eb638a09b62bc
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-09-11 18:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return '@dxziyan'
+class Greeting (Resource):
+    def get(self):
+        return "Bixby Rename bot is Up & Running!"
 
-
-if __name__ == "__main__":
-    app.run()
+api.add_resource(Greeting, '/')
+app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
