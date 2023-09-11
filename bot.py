535b98ba41927895de05b9c861f8ea748f844b07
ZIYAN <92292759+DX-MODS@users.noreply.github.com>
2023-09-11 18:38:56 +0530

    Fix✅

--- bot.py
+++ bot.py
@@ -1,56 +1,24 @@
-# Copyright (C) 2023 DX_MODS
-#Licensed under the  AGPL-3.0 License;
-#you may not use this file except in compliance with the License.
-#Author ZIYAN
-from datetime import datetime
-from pytz import timezone
-from pyrogram import Client, __version__
-from pyrogram.raw.all import layer
+import asyncio
+from pyrogram import Client, compose,idle
+import os
+
 from plugins.cb_data import app as Dxbot
 from config import Config
-from aiohttp import web
-from route import web_server
-import asyncio
-from helper.ping_server import ping_server
 
 
-class Bot(Client):
 
-    def __init__(self):
-        super().__init__(
-            name="renamer",
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            bot_token=Config.BOT_TOKEN,
-            workers=200,
-            plugins={"root": "plugins"},
-            sleep_threshold=15,
-        )
+Bot = Client(
 
-    async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
-        if Config.WEBHOOK:
-            asyncio.create_task(ping_server())
-            app = web.AppRunner(await web_server())
-            await app.setup()
-            bind_address = "0.0.0.0"
-            await web.TCPSite(app, bind_address, Config.PORT).start()     
-        print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
-        for id in Config.ADMIN:
-            try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
-            except: pass
-        if Config.LOG_CHANNEL:
-            try:
-                curr = datetime.now(timezone("Asia/Kolkata"))
-                date = curr.strftime('%d %B, %Y')
-                time = curr.strftime('%I:%M:%S %p')
-                await self.send_message(Config.LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
-            except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
+           "Bixby",
+
+           bot_token=Config.BOT_TOKEN,
+
+           api_id=Config.API_ID,
+
+           api_hash=Config.API_HASH,
+
+           plugins=dict(root='plugins'))
+           
 
 if Config.PYROGRAM_SESSION:
     apps = [Dxbot,Bot]
@@ -61,4 +29,4 @@ if Config.PYROGRAM_SESSION:
         app.stop()
     
 else:
-    Bot().run()
+    Bot.run()
